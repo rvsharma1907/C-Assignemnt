9f6f873a6dc4c486f70d2c488ceae8ee519cd7a8
rvsharma1907 <102277692+rvsharma1907@users.noreply.github.com>
2022-04-12 10:20:09 +0530

    Create 5-3.cs

---
+++ 5-C#-CollectionFramework Assignmenst/5-3.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+class Employee
+{
+    int empId;
+    string empName;
+    string empdept;
+    public Employee(int id, string name, string dept)
+    {
+        empId = id;
+        empName = name;
+        empdept = dept;
+    }
+    public int EmplId()
+    {
+        return empId;
+    }
+    public string EmplName()
+    {
+        return empName;
+    }
+    public string EmpDept()
+    {
+        return empdept;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayList empList = new ArrayList();
+        while (true)
+        {
+            Console.WriteLine("1.ENTER EMPLOYEE DETAILS \n2. DISPLAY EMPLOYEE DETAILS \n3. TOTAL NUMBER OF EMPLOYEE\n4. EXIT");
+            Console.WriteLine("Enter your choice : ");
+            int choice = int.Parse(Console.ReadLine());
+            if (choice == 1)
+            {
+                Console.WriteLine("Enter Employee Id : ");
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Employee Name : ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter Employee Dept : ");
+                string dept = Console.ReadLine();
+                empList.Add(new Employee(id, name, dept));
+
+            }
+            else if (choice == 2)
+            {
+                foreach (Employee emp in empList)
+                {
+                    Console.WriteLine("Employee ID : {0}, Employee Name : {1}, Employee Dept : {2}", emp.EmplId(), emp.EmplName(), emp.EmpDept());
+                }
+            }
+            else if (choice == 3)
+            {
+                Console.WriteLine("Total number of employees : " + empList.Count);
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice");
+                break;
+            }
+        }
+    }
+}
